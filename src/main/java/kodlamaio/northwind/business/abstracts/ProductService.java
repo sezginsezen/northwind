b9f8a89af7156f9a4877a25595bb53e882346689
sezginsezen <szgszn@yandex.com>
2021-05-26 16:20:15 +0300

    Commit

--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -6,9 +6,7 @@ import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
-public interface ProductService
-{
+public interface ProductService {
     DataResult<List<Product>> getAll();
-
     Result add(Product product);
 }
